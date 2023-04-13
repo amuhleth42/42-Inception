7cf15ab969ebf56c2920724369d7cd26e9bce440
Aymeric Muhlethaler <96829299+amuhleth42@users.noreply.github.com>
2023-04-13 19:16:54 +0200

    allez

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -7,4 +7,5 @@ mysql -u root -e "CREATE DATABASE $WP_DB_NAME;"
 mysql -u root -e "USE $WP_DB_NAME; GRANT ALL PRIVILEGES ON * TO '$WP_DB_USR'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 #mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PWD';"
 
+echo "mariadb started !"
 exec mysqld
--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -1,34 +1,20 @@
 # wait for mariadb
+#while ! mariadb -h$MARIADB_HOST -u$WP_DB_USR -p$WP_DB_PWD $WP_DB_NAME &>/dev/null; do
+	#echo "waiting for db ..."
+	sleep 6
+#done
 
-while ! mariadb -h$MARIADB_HOST -u$WP_DB_USR -p$WP_DB_PWD $WP_DB_NAME &>/dev/null; do
-	echo "waiting for db ..."
-	sleep 3
-done
+echo "on sort du whiled"
 
-# check if already installed
-
-if [ -f /var/www/wordpress/wp-config.php ]
-then
-	echo "wordpress already downloaded"
-else
-
-	# install wp cli
-
-	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-	chmod +x wp-cli.phar
-	mv wp-cli.phar /usr/bin/wp
-
-	# install wordpress
+#sleep infinity
 
 	cd /var/www/wordpress
-	wp core download --allow-root
 
-	echo yo
-	wp core config --dbhost=$MARIADB_HOST	\
-					--dbname=$WP_DB_NAME 	\
-					--dbuser=$WP_DB_USER	\
-					--dbpass=$WP_DB_PWD 	\
-					--allow-root
+	sed -i "s/username_here/$WP_DB_USR/g" wp-config-sample.php
+	sed -i "s/password_here/$WP_DB_PWD/g" wp-config-sample.php
+	sed -i "s/localhost/$MARIADB_HOSTNAME/g" wp-config-sample.php
+	sed -i "s/database_name_here/$WP_DB_NAME/g" wp-config-sample.php
+	cp wp-config-sample.php wp-config.php
 
 	echo yo
 	wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USR \
@@ -37,8 +23,5 @@ else
 	echo yo
 	wp user create $WP_USR $USR_EMAIL --role=author --user_pass=$WP_PWD --allow-root
 
-fi
-
 # start php-fpm
-
 
